59493590e64b38e9728102b4a1ecb53fbedb104a
axelm3ndez <50780680+axelm3ndez@users.noreply.github.com>
2025-09-06 15:22:17 -0300

    Add files via upload

--- migrations.sql
+++ migrations.sql
@@ -2,21 +2,16 @@
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   email TEXT UNIQUE NOT NULL,
-  password_hash TEXT NOT NULL,
+  password TEXT NOT NULL,
   name TEXT,
-  slug TEXT UNIQUE,
-  phone TEXT,
-  city TEXT,
+  gender TEXT,
   description TEXT,
-  destacado_until TIMESTAMP NULL,
   created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS payments (
   id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id),
-  preference_id TEXT,
-  amount INTEGER,
+  user_id INT REFERENCES users(id),
   status TEXT,
   created_at TIMESTAMP DEFAULT NOW()
 );
